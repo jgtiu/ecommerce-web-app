eb179b852e1869b8efabe428ea9c6be0af22e4af
jess <tjessica10@gmail.com>
2022-12-01 16:17:16 -0500

    Create Order type

--- server/data.ts
+++ server/data.ts
@@ -10,3 +10,12 @@ export interface Product extends DraftProduct {
   sellerId: string
 }
 
+export interface Order {
+  _id: string // Note that order ID is different from product ID
+  productName: string
+  productPrice: number
+  productAllowReturns: boolean
+  productId: string
+  productSellerId: string
+  fulfilled: boolean
+}
