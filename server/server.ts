b31f68442131211a558aa21ece6242d881055049
jess <tjessica10@gmail.com>
2022-12-01 11:36:37 -0500

    Add backend route for getting all products and product info

--- server/server.ts
+++ server/server.ts
@@ -92,8 +92,10 @@ app.post(
   }
 )
 
-app.get("/api/orders", async (req, res) => {
-  res.status(200).json(await orders.find({ state: { $ne: "draft" }}).toArray())
+app.get("/products", async (req, res) => {
+  // returns all products and product info
+  //primarily for admin page
+  res.status(200).json(await products.find().toArray())
 })
 
 app.get("/api/user", (req, res) => {
