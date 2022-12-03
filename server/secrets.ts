9c89039ed50ebea9dae29e625c0e76c6cb11acc6
Zhe Wang <zhe.wang1998@duke.edu>
2022-12-03 16:05:48 -0500

    solve merge conflict

--- server/secrets.ts
+++ server/secrets.ts
@@ -1,7 +1,7 @@
 export const keycloak = {
   client_id: "webapp",
   client_secret: "ttSySTcpXke2y4AXPfiUahp3vE4X2vMB", // TODO
-  redirect_uris: ["http://127.0.0.1:8080/api/login-callback"],
+  redirect_uris: ["http://127.0.0.1:8082/api/login-callback"],
   post_logout_redirect_uris: [""],
   response_types: ["code"],
 }
