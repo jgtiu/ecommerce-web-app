068698ce1b5aca23e122419dd3cc983d72ce2e5a
jess <tjessica10@gmail.com>
2022-12-04 16:18:51 -0500

    Remove hyperlinks we do not need

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -7,9 +7,6 @@
         <span v-else>E-commerce site</span>
       </b-navbar-brand>
       <b-navbar-nav>
-        <b-nav-item href="/">All Orders</b-nav-item>
-        <b-nav-item v-if="user?.roles?.includes('customer')" href="/customer">My Orders</b-nav-item>
-        <b-nav-item v-if="user?.roles?.includes('operator')" href="/operator">My Work Screen</b-nav-item>
         <b-nav-item v-if="user?.name == null" href="/api/login">Login</b-nav-item>
         <b-nav-item v-if="user?.name" @click="logout">Logout</b-nav-item>
         <form method="POST" action="/api/logout" id="logoutForm" />
