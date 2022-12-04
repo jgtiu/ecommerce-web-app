42971a8a435887da2c8212a90dc3a0cd65d2824a
jess <tjessica10@gmail.com>
2022-12-04 18:21:31 -0500

    Add a refresh button

--- ui/src/views/Products.vue
+++ ui/src/views/Products.vue
@@ -1,9 +1,22 @@
 <template>
   <div class="mx-3 my-3">
     <h2>My Products</h2>
+    <b-button @click="refresh" class="mb-2">Refresh</b-button>
   </div>
 </template>
 
 <script setup lang="ts">
+import { watch, ref, inject, Ref } from 'vue'
+import { SellerWithOtherAttributes } from '../../../server/data';
+
+const seller: Ref<SellerWithOtherAttributes | null> = ref(null)
+const user: Ref<any> = inject("user")!
+
+async function refresh() {
+  if (user.value) {
+    seller.value = await (await fetch("/api/seller")).json()
+  }
+}
+watch(user, refresh, { immediate: true })
 
 </script>
