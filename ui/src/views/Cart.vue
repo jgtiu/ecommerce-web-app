9b00667ce9737117c83b7a9fc5bca9b0f92daa77
jess <tjessica10@gmail.com>
2022-12-05 01:43:57 -0500

    Add refresh function

--- ui/src/views/Cart.vue
+++ ui/src/views/Cart.vue
@@ -5,5 +5,23 @@
 </template>
 
 <script setup lang="ts">
+import { watch, ref, inject, Ref } from 'vue'
+import { BuyerWithOrders, Order } from "../../../server/data"
+
+const buyer: Ref<BuyerWithOrders | null>= ref(null)
+const cart: Ref<Order[]> = ref([])
+const user: Ref<any> = inject("user")!
+
+async function refresh() {
+  if (user.value) {
+    buyer.value = await (await fetch("/api/buyer")).json()
+  }
+  if (buyer.value) {
+    cart.value = buyer.value.orders.filter((element: Order) => {
+      return element.state === "cart"
+    })
+  }
+}
+watch(user, refresh, { immediate: true })
 
 </script>
