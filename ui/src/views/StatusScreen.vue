0174b238dac18b7c12251464ff8f082b26b2da92
jess <tjessica10@gmail.com>
2022-12-05 03:16:25 -0500

    Display products

--- ui/src/views/StatusScreen.vue
+++ ui/src/views/StatusScreen.vue
@@ -2,7 +2,13 @@
   <div class="mx-3 my-3">
     <h2>Admin</h2>
     <b-button @click="refresh" class="mb-2">Refresh</b-button>
-    <b-table :items="orders" />
+    <b-table :items="products" :fields="fields">
+      <template #cell(editPrice)="cellScope">
+        <b-button @click="changeEditState(cellScope.item)">
+          Edit Price
+        </b-button>
+      </template>
+    </b-table>
   </div>
 </template>
 
@@ -11,6 +17,7 @@ import { onMounted, ref, Ref } from 'vue'
 import { Product } from '../../../server/data'
 
 const products: Ref<Product[]> = ref([])
+const formState: Ref<boolean> = ref(false)
 const productBeingEdited: Ref<Product | null> = ref(null)
 const newPrice: Ref<number> = ref(0)
 
@@ -19,4 +26,11 @@ async function refresh() {
   newPrice.value = 0
 }
 onMounted(refresh)
+
+const fields = ["name", "description", "allowReturns", "sellerId", "price", "editPrice"]
+
+async function changeEditState(product: Product) {
+  formState.value = true
+  productBeingEdited.value = product
+}
 </script>
