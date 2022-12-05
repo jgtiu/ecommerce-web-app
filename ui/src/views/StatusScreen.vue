01f054ec5369ae0336ff3a27c164754cb9435ade
jess <tjessica10@gmail.com>
2022-12-05 03:16:47 -0500

    Add a toggling form to edit prices

--- ui/src/views/StatusScreen.vue
+++ ui/src/views/StatusScreen.vue
@@ -2,6 +2,22 @@
   <div class="mx-3 my-3">
     <h2>Admin</h2>
     <b-button @click="refresh" class="mb-2">Refresh</b-button>
+
+    <b-form v-if="formState">
+      <h3>Edit Price</h3>
+      Product being edited: {{ { _id: productBeingEdited._id, name: productBeingEdited.name } }}
+      <br>
+      <br>
+      <b-form-group label="New Price:" label-for="product-price-input">
+        <b-form-input id="product-price-input" type="number" :number="true" v-model="newPrice" />
+      </b-form-group>
+      <b-button @click="save(productBeingEdited._id)">Save</b-button>
+      <br>
+      <br>
+      <b-button @click="closeForm">Close Form</b-button>
+      <br>
+    </b-form>
+
     <b-table :items="products" :fields="fields">
       <template #cell(editPrice)="cellScope">
         <b-button @click="changeEditState(cellScope.item)">
@@ -33,4 +49,24 @@ async function changeEditState(product: Product) {
   formState.value = true
   productBeingEdited.value = product
 }
+
+async function save(productId: string) {
+  await fetch(
+    "/api/product/" + productId + "/edit",
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "PUT",
+      body: JSON.stringify({ price: newPrice.value })
+    }
+  )
+  await refresh()
+}
+
+async function closeForm() {
+  formState.value = false
+  await refresh()
+}
+
 </script>
